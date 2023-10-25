741901c1ca3ca93b7a2d728b23ba835c5c773899
Fiedor_ <stachfie@wp.pl>
2023-10-25 17:33:34 +0200

    Yes

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Wizytówki</title>
+    <link rel="stylesheet" href="styl4.css">
+</head>
+
+<body>
+    <header>
+
+    </header>
+</body>
+
+</html>
