0ef0f09ef2d2ef47e7bfbba88a45b79cd6ac3d1b
Fiedor_ <stachfie@wp.pl>
2023-10-26 21:05:23 +0200

    DONE

--- index.php
+++ index.php
@@ -11,13 +11,25 @@
     <header>
         <h1>Wizytówki pracowników</h1>
         <form action="index.php" method="post">
-            <input type="number" value='1' min='1' max='9' >
+            <input type="number" value='1' min='1' max='9' name='id'>
             <input type="submit" value='Wyświetl'>
         </form>
     </header>
         <div id='wizyt'>
         <?php
-        //skrypt1
+        $conn=mysqli_connect('localhost','root','','firma');
+        if(isset($_POST['id'])){
+            $id=$_POST['id'];
+        }else{
+            $id=1;
+        }
+        $result=mysqli_query($conn,"SELECT id,imie,nazwisko,adres,miasto FROM pracownicy where id=$id");
+        foreach ($result as $r){
+            echo "<img src='". $r["id"].".jpg' alt='pracownik'>";
+            echo "<h2>". $r['imie'].'&nbsp'.$r['nazwisko']."</h2>";
+            echo '<h4>Adres:</h4>';
+            echo "<p>".$r['adres'].' , '.$r['miasto']."</p>";
+        }
         ?>
         </div>
     <footer>
@@ -31,7 +43,13 @@
         <div id='right'>
             <p>Osoby proszone o podpisanie dokumentu RODO:</p>
             <?php
-            //skrypt2
+            $result2=mysqli_query($conn,"SELECT imie, nazwisko FROM pracownicy WHERE czyRODO=0;");
+            echo "<ol>";
+            foreach($result2 as $res){
+                    echo '<li>'.$res['imie']."&nbsp".$res['nazwisko'].'</li>';
+            }
+            echo "</ol>";
+            mysqli_close($conn);
             ?>
         </div>
     </footer>
